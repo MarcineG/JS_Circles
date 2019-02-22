3fafe9b26f7cca709110529ebfa671f5eda259ab
marcin <marcin15g@gmail.com>
2019-02-22 16:15:42 +0100

    Change colors on collision with walls

--- script.js
+++ script.js
@@ -9,18 +9,31 @@ function Circle(x,y,dx,dy,radius) {
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
+    this.fillColor = "black";
+    this.strokeColor = "black";
 
     this.draw = function() {
         c.beginPath();
         c.arc(this.x+this.dx,this.y+this.dy,1+this.radius,0,Math.PI*2,false);
-        c.strokeStyle = "aqua";
+        c.strokeStyle = this.strokeColor;
+        c.fillStyle = this.fillColor;
         c.stroke();
+        c.fill();
     };
-
+    this.switchColors = function() {
+        this.fillColor = "#"+((1<<24)*Math.random()|0).toString(16);
+        this.strokeColor = "#"+((1<<24)*Math.random()|0).toString(16);
+    };
     this.update = function() {
         //Bounce of walls
-        if(this.x + this.radius > innerWidth || this.x - this.radius <= 0) {this.dx = -this.dx;}
-        if(this.y + this.radius > innerHeight || this.y - this.radius <= 0) {this.dy = -this.dy;}
+        if(this.x + this.radius > innerWidth || this.x - this.radius <= 0) {
+            this.dx = -this.dx;
+            this.switchColors();
+        }
+        if(this.y + this.radius > innerHeight || this.y - this.radius <= 0) {
+            this.dy = -this.dy;
+            this.switchColors();
+        }
         this.x+=this.dx;
         this.y+=this.dy;
         this.draw();
@@ -28,7 +41,7 @@ function Circle(x,y,dx,dy,radius) {
 }
 
 let circleArray = [];
-let amount = 300;
+let amount = 200;
 
 for(let i=0; i<amount+1; i++) {
     let radius=Math.random()*50+5,
@@ -36,7 +49,10 @@ for(let i=0; i<amount+1; i++) {
         dx=(Math.random() - 0.5) * 4,
         dr=0,
         x = Math.random() * (innerWidth - radius * 2) + radius,
-        y = Math.random() * (innerHeight - radius * 2) + radius;
+        y = Math.random() * (innerHeight - radius * 2) + radius,
+        fill = "#"+((1<<24)*Math.random()|0).toString(16),
+        stroke = "#"+((1<<24)*Math.random()|0).toString(16);
+
         circleArray.push(new Circle(x,y,dx,dy,radius));
 }
 
