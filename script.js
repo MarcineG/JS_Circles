76bab7d1135e46a84f7c707f42f52e25a0aaf4c1
marcin <marcin15g@gmail.com>
2019-02-22 14:08:59 +0100

    Circle object

--- script.js
+++ script.js
@@ -2,48 +2,52 @@ let canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let c = canvas.getContext('2d');
-/*
-c.fillStyle = 'blue';
-c.fillRect(100,100,100,100);
 
-//Line
-c.beginPath();
-c.moveTo(50,300);
-c.lineTo(300,100);
-c.lineTo(200,300);
-c.strokeStyle = "red";
-c.stroke();
 
-//Arcs
-for(let i = 0; i<=100 ; i+=10)
-{
-    c.beginPath();
-    c.strokeStyle = "green";
-    c.arc(200+i,200+i,30+i,0,Math.PI*2,false);
-    c.stroke();
+let radius=40,
+    maxRadius = 40,
+    dy=(Math.random() - 0.5) * 10,
+    dx=(Math.random() - 0.5) * 10,
+    dr=0,
+    x = Math.random() * innerWidth,
+    y = Math.random() * innerHeight;
+
+
+
+
+
+function Circle(x,y,dx,dy,radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+
+    this.draw = function() {
+        c.beginPath();
+        c.arc(this.x+this.dx,this.y+this.dy,1+this.radius,0,Math.PI*2,false);
+        c.strokeStyle = "aqua";
+        c.stroke();
+    };
+
+    this.update = function() {
+        //Bounce of walls
+        if(this.x + this.radius > innerWidth || this.x <= 0) {this.dx = -this.dx;}
+        if(this.y + this.radius > innerHeight || this.y <= 0) {this.dy = -this.dy;}
+        this.x+=this.dx;
+        this.y+=this.dy;
+        this.draw();
+    }
 }
-for(let i = 0; i<=50 ; i+=1)
-{
-    c.beginPath();
-    let x = Math.random()*window.innerWidth;
-    let y = Math.random()*window.innerHeight;
-    c.strokeStyle = "aqua";
-    c.arc(x,y,30+i,0,Math.PI*2,false);
-    c.stroke();
-}*/
 
-let x=1,y=1;
-    requestAnimationFrame(animate);
 
+let first = new Circle(200,200,4,4,40);
+
+requestAnimationFrame(animate);
 
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0,0,innerWidth,innerHeight);
-    c.beginPath();
-    c.arc(400+y,400,1+x,0,Math.PI*2,false);
-    c.strokeStyle = "aqua";
-    c.stroke();
-    if(x > 50) {x=0;}
-    else {x+=2;}
-    y++;
+    first.update();
+
 }
